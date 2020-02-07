8541967990df4eafb903a07757ae36433e6bff73
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-02-07 13:27:48 +0100

    Updates

---
+++ precache-manifest.b8bbf05dea1250854d764c0f6c9cc327.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0baa237795875b497a97734b54a13994",
+    "url": "/book-library-react/index.html"
+  },
+  {
+    "revision": "2dc25f145fc307278d03",
+    "url": "/book-library-react/static/js/2.9bc19038.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/book-library-react/static/js/2.9bc19038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d271855f0e3fe18c678b",
+    "url": "/book-library-react/static/js/main.ea490350.chunk.js"
+  },
+  {
+    "revision": "35e01341d4dc8d9c3da3",
+    "url": "/book-library-react/static/js/runtime-main.3523973e.js"
+  }
+]);
